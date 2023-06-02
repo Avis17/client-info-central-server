ac4a72facec92ab6c1b80d9d093844a6fd8b04e3
siva <pvadivelsiva@gmail.com>
2023-06-02 23:00:02 +0530

    updated multi request handling

--- index.js
+++ index.js
@@ -1,34 +1,93 @@
-const express = require("express")
-const app = express()
-const bodyParser = require("body-parser");
-const cors = require("cors")
-const authenticationRoute = require("./routes/authentication");
-const appMetaCreaton = require("./routes/app-meta-creation");
-const authMiddleware = require("./middlewares/authMiddleware");
+// const express = require("express")
+// const app = express()
+// const bodyParser = require("body-parser");
+// const cors = require("cors")
+// const authenticationRoute = require("./routes/authentication");
+// const appMetaCreaton = require("./routes/app-meta-creation");
+// const authMiddleware = require("./middlewares/authMiddleware");
+// const PORT = process.env.PORT || 2000;
+// const mongo = require("./utils/dao");
+// const path = require('path');
+// const entitiesRouter = require('./routes/entity-router');
+
+// // middlewares
+// app.use(cors())
+// app.use(bodyParser({extended:true}))
+// app.use(express.static(path.join(__dirname, 'public'))); 
+// app.use(authenticationRoute)
+
+// mongo.connect().then((result)=>{
+//     console.log(result)
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
+// app.get("/", (req, res)=>{
+//     res.sendFile(path.join(public, 'index.html'));
+// })
+// app.use(authMiddleware);
+// app.use("/app-meta-creation", appMetaCreaton)
+// app.use('/entities', entitiesRouter);
+
+// app.listen(PORT, ()=>{
+//     console.log("CLC server is running on PORT "+PORT)
+// })
+
+
+// for multiple request handling code
+
+const cluster = require('cluster');
+const os = require('os');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const authenticationRoute = require('./routes/authentication');
+const appMetaCreaton = require('./routes/app-meta-creation');
+const authMiddleware = require('./middlewares/authMiddleware');
 const PORT = process.env.PORT || 2000;
-const mongo = require("./utils/dao");
+const mongo = require('./utils/dao');
 const path = require('path');
 const entitiesRouter = require('./routes/entity-router');
 
-// middlewares
-app.use(cors())
-app.use(bodyParser({extended:true}))
-app.use(express.static(path.join(__dirname, 'public'))); 
-app.use(authenticationRoute)
+const numCPUs = os.cpus().length;
 
-mongo.connect().then((result)=>{
-    console.log(result)
-}).catch((err)=>{
-    console.log(err)
-})
+// Check if the current process is the master process
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
 
-app.get("/", (req, res)=>{
+  // Fork workers
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
+} else {
+  // Workers share the TCP connection in this case
+  app.use(cors());
+  app.use(bodyParser({ extended: true }));
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(authenticationRoute);
+
+  mongo
+    .connect()
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  app.get('/', (req, res) => {
     res.sendFile(path.join(public, 'index.html'));
-})
-app.use(authMiddleware);
-app.use("/app-meta-creation", appMetaCreaton)
-app.use('/entities', entitiesRouter);
+  });
+  app.use(authMiddleware);
+  app.use('/app-meta-creation', appMetaCreaton);
+  app.use('/entities', entitiesRouter);
 
-app.listen(PORT, ()=>{
-    console.log("CLC server is running on PORT "+PORT)
-})
+  app.listen(PORT, () => {
+    console.log(`Worker ${process.pid} is running on PORT ${PORT}`);
+  });
+}
--- routes/entity-router.js
+++ routes/entity-router.js
@@ -1,11 +1,141 @@
+// const express = require('express');
+// const entitiesRouter = express.Router();
+// const ConnectionManager = require('../utils/connection-manager');
+// const mongoose = require('mongoose');
+// const crypto = require("./crypro");
+
+// // Create an instance of the ConnectionManager
+// const connectionManager = new ConnectionManager();
+
+// // Create an entity
+// entitiesRouter.post('/', async (req, res) => {
+//   try {
+//     const dbName = req.body.dbName;
+//     const collectionName = req.body.collectionName;
+//     const entitySchema = req.body.schema;
+//     const collectionData = req.body.collectionData;
+//     const dbUrl = `mongodb://127.0.0.1:27017/${dbName}`;
+//     const options = {
+//       //   useNewUrlParser: true,
+//       //   useUnifiedTopology: true
+//     };
+//     connectionManager.getConnection(dbName, dbUrl, options).then(async (connection) => {
+//       const Entity = connection.model(collectionName, new mongoose.Schema({}, { strict: false, timestamps: true }));
+//       // console.log(Entity)
+//       const newEntity = new Entity(collectionData);
+//       try {
+//         const savedEntity = await newEntity.save();
+//         // Release the connection
+//         await connectionManager.releaseConnection(dbName);
+//         res.status(200).json({ status: 200, data: crypto.encrypt(JSON.stringify(savedEntity)) });
+//       } catch (error) {
+//         // Handle specific error types
+//         await connectionManager.releaseConnection(dbName);
+//         if (error.code === 11000) {
+//           // Duplicate key error
+//           return res.status(409).json({
+//             status: 409,
+//             message: error
+//           });
+//         }
+//         // Handle other errors
+//         res.status(400).json({ status: 400, message: error });
+//       }
+//     }).catch((err) => {
+//       res.status(500).json({
+//         status: 500, message: err
+//       });
+//     })
+
+//   } catch (error) {
+//     res.status(500).json({
+//       status: 500, message: error
+//     });
+//   }
+// });
+
+// // Read all entities
+// entitiesRouter.post('/get-all-entities', async (req, res) => {
+//   try {
+//     const dbName = req.body.dbName;
+//     const collectionName = req.body.collectionName;
+//     const entitySchema = req.body.schema;
+//     const querydata = req.body.queryData;
+//     const dbUrl = `mongodb://127.0.0.1:27017/${dbName}`;
+//     const options = {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//     };
+//     const connection = await connectionManager.getConnection(dbName, dbUrl, options);
+//     const Entity = connection.model(collectionName, new mongoose.Schema(entitySchema, { timestamps: true }));
+//     const resData = await Entity.find(querydata);
+
+//     // Release the connection
+//     await connectionManager.releaseConnection(dbName);
+
+//     res.status(200).json({ status: 200, data: crypto.encrypt(JSON.stringify(resData)) });
+//   } catch (error) {
+//     res.status(500).json({ status: 500, message:error });
+//   }
+// });
+
+// // Update an entity
+// entitiesRouter.put('/update-entity-by-id/:id', async (req, res) => {
+//   try {
+//     const dbName = req.body.dbName;
+//     const collectionName = req.body.collectionName;
+//     const entitySchema = req.body.schema;
+//     const collectionData = req.body.collectionData;
+//     const dbUrl = `mongodb://127.0.0.1:27017/${dbName}`;
+//     const options = {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//     };
+//     const connection = await connectionManager.getConnection(dbName, dbUrl, options);
+//     const Entity = connection.model(collectionName, new mongoose.Schema(entitySchema, { timestamps: true }));
+//     const resData = await Entity.findByIdAndUpdate(req.params.id, collectionData, { new: true });
+//     // Release the connection
+//     await connectionManager.releaseConnection(dbName);
+//     if (!resData) {
+//       return res.status(404).json({ status: 409, error: 'Entity not found' });
+//     }
+//     res.status(200).json({ status: 200, data: crypto.encrypt(JSON.stringify(resData)) });
+//   } catch (error) {
+//     res.status(500).json({ status: 500, message:error });
+//   }
+// });
+
+// // Delete an entity
+// entitiesRouter.delete('/delete-entity-by-id/:id', async (req, res) => {
+//   try {
+//     const dbName = req.body.dbName;
+//     const collectionName = req.body.collectionName;
+//     const entitySchema = req.body.schema;
+//     const dbUrl = `mongodb://127.0.0.1:27017/${dbName}`;
+//     const options = {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//     };
+//     const connection = await connectionManager.getConnection(dbName, dbUrl, options);
+//     const Entity = connection.model(collectionName, new mongoose.Schema(entitySchema, { timestamps: true }));
+//     const resData = await Entity.findByIdAndDelete(req.params.id);
+//     // Release the connection
+//     await connectionManager.releaseConnection(dbName);
+//     if (!resData) {
+//       return res.status(404).json({ status: 404, error: 'Entity not found' });
+//     }
+//     res.status(200).json({ status: 200, data: crypto.encrypt('Entity deleted successfully') });
+//   } catch (error) {
+//     res.status(500).json({ status: 500, message:error });
+//   }
+// });
+
+// module.exports = entitiesRouter;
+
 const express = require('express');
 const entitiesRouter = express.Router();
-const ConnectionManager = require('../utils/connection-manager');
 const mongoose = require('mongoose');
 const crypto = require("./crypro");
-
-// Create an instance of the ConnectionManager
-const connectionManager = new ConnectionManager();
 
 // Create an entity
 entitiesRouter.post('/', async (req, res) => {
@@ -19,34 +149,24 @@ entitiesRouter.post('/', async (req, res) => {
       //   useNewUrlParser: true,
       //   useUnifiedTopology: true
     };
-    connectionManager.getConnection(dbName, dbUrl, options).then(async (connection) => {
-      const Entity = connection.model(collectionName, new mongoose.Schema({}, { strict: false, timestamps: true }));
-      // console.log(Entity)
-      const newEntity = new Entity(collectionData);
-      try {
-        const savedEntity = await newEntity.save();
-        // Release the connection
-        connectionManager.releaseConnection(dbName);
-        res.status(200).json({ status: 200, data: crypto.encrypt(JSON.stringify(savedEntity)) });
-      } catch (error) {
-        // Handle specific error types
-        connectionManager.releaseConnection(dbName);
-        if (error.code === 11000) {
-          // Duplicate key error
-          return res.status(409).json({
-            status: 409,
-            message: error
-          });
-        }
-        // Handle other errors
-        res.status(400).json({ status: 400, message: error });
+    const connection = mongoose.createConnection(dbUrl, options);
+    const db = connection.useDb(dbName);
+    const Entity = db.model(collectionName, new mongoose.Schema({}, { strict: false, timestamps: true }));
+    const newEntity = new Entity(collectionData);
+    try {
+      const savedEntity = await newEntity.save();
+      res.status(200).json({ status: 200, data: crypto.encrypt(JSON.stringify(savedEntity)) });
+    } catch (error) {
+      if (error.code === 11000) {
+        // Duplicate key error
+        return res.status(409).json({
+          status: 409,
+          message: error
+        });
       }
-    }).catch((err) => {
-      res.status(500).json({
-        status: 500, message: error
-      });
-    })
-
+      // Handle other errors
+      res.status(400).json({ status: 400, message: error });
+    }
   } catch (error) {
     res.status(500).json({
       status: 500, message: error
@@ -66,16 +186,13 @@ entitiesRouter.post('/get-all-entities', async (req, res) => {
       useNewUrlParser: true,
       useUnifiedTopology: true
     };
-    const connection = await connectionManager.getConnection(dbName, dbUrl, options);
-    const Entity = connection.model(collectionName, new mongoose.Schema(entitySchema, { timestamps: true }));
+    const connection = mongoose.createConnection(dbUrl, options);
+    const db = connection.useDb(dbName);
+    const Entity = db.model(collectionName, new mongoose.Schema({}, { strict: false, timestamps: true }));
     const resData = await Entity.find(querydata);
-
-    // Release the connection
-    connectionManager.releaseConnection(dbName);
-
     res.status(200).json({ status: 200, data: crypto.encrypt(JSON.stringify(resData)) });
   } catch (error) {
-    res.status(500).json({ status: 500, message:error });
+    res.status(500).json({ status: 500, message: error });
   }
 });
 
@@ -91,17 +208,16 @@ entitiesRouter.put('/update-entity-by-id/:id', async (req, res) => {
       useNewUrlParser: true,
       useUnifiedTopology: true
     };
-    const connection = await connectionManager.getConnection(dbName, dbUrl, options);
-    const Entity = connection.model(collectionName, new mongoose.Schema(entitySchema, { timestamps: true }));
+    const connection = mongoose.createConnection(dbUrl, options);
+    const db = connection.useDb(dbName);
+    const Entity = db.model(collectionName, new mongoose.Schema({}, { strict: false, timestamps: true }));
     const resData = await Entity.findByIdAndUpdate(req.params.id, collectionData, { new: true });
-    // Release the connection
-    connectionManager.releaseConnection(dbName);
     if (!resData) {
       return res.status(404).json({ status: 409, error: 'Entity not found' });
     }
     res.status(200).json({ status: 200, data: crypto.encrypt(JSON.stringify(resData)) });
   } catch (error) {
-    res.status(500).json({ status: 500, message:error });
+    res.status(500).json({ status: 500, message: error });
   }
 });
 
@@ -116,17 +232,16 @@ entitiesRouter.delete('/delete-entity-by-id/:id', async (req, res) => {
       useNewUrlParser: true,
       useUnifiedTopology: true
     };
-    const connection = await connectionManager.getConnection(dbName, dbUrl, options);
-    const Entity = connection.model(collectionName, new mongoose.Schema(entitySchema, { timestamps: true }));
+    const connection = mongoose.createConnection(dbUrl, options);
+    const db = connection.useDb(dbName);
+    const Entity = db.model(collectionName, new mongoose.Schema({}, { strict: false, timestamps: true }));
     const resData = await Entity.findByIdAndDelete(req.params.id);
-    // Release the connection
-    connectionManager.releaseConnection(dbName);
     if (!resData) {
       return res.status(404).json({ status: 404, error: 'Entity not found' });
     }
     res.status(200).json({ status: 200, data: crypto.encrypt('Entity deleted successfully') });
   } catch (error) {
-    res.status(500).json({ status: 500, message:error });
+    res.status(500).json({ status: 500, message: error });
   }
 });
 
