ead2341b2a42de2c5158e1fc9eb16126e999a4c2
siva <pvadivelsiva@gmail.com>
2023-06-17 18:02:16 +0530

    updated UI code

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ if (cluster.isMaster) {
     });
 
   app.get('/', (req, res) => {
-    res.sendFile(path.join(public, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
 
   app.get('/files/:dbName/:collectionName/:entityId/download', async (req, res) => {
@@ -86,15 +86,19 @@ if (cluster.isMaster) {
     }
   });
 
+  app.use((req, res, next) => {
+    if (req.path === '/dashboard') {
+      res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    }else {
+      next(); // Pass control to the next middleware/route handler
+    }
+  });
+
   app.use(authMiddleware);
   app.use('/app-meta-creation', appMetaCreaton);
   app.use('/app/wfm/v1/entities', wfmEntityRouter);
   app.use('/entities', entitiesRouter);
 
-  // Catch-all route for invalid routes
-  app.use((req, res, next) => {
-    res.redirect('/'); // Redirect to the login route
-  });
   app.listen(PORT, () => {
     console.log(`Worker ${process.pid} is running on PORT ${PORT}`);
   });
