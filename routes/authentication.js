41167f614187e0ab8a17009988c61e059008d730
siva <pvadivelsiva@gmail.com>
2023-07-14 00:41:48 +0530

    updated ui

--- routes/authentication.js
+++ routes/authentication.js
@@ -161,7 +161,8 @@ router.post('/forgot-password', async (req, res) => {
     await user.save();
 
     // Create a password reset URL with the token
-    const resetUrl = `https://customer-info-central.onrender.com/reset-password?token=${resetToken}`;
+    // const resetUrl = `https://customer-info-central.onrender.com/reset-password?token=${resetToken}`;
+    const resetUrl = `http://localhost:2000/reset-password?token=${resetToken}`;
 
     // Send the password reset email
     const transporter = nodemailer.createTransport({
@@ -198,6 +199,7 @@ router.post('/forgot-password', async (req, res) => {
 
 router.post('/api/reset-password', async (req, res) => {
   try {
+    console.log(req.body);
     const { token, password } = req.body;
     // Find the user with the provided reset token
     const user = await authentiaction.findOne({
